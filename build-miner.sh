331d6cb87511b79af65d885da02cafb1975d8d4d
Open BaseBox.org <baseboxorg@users.noreply.github.com>
2017-05-23 14:17:02 +0800

    Update build-miner.sh

--- build-miner.sh
+++ build-miner.sh
@@ -3,7 +3,7 @@
 echo "building miner"
 cd /miner
 apt-get update && apt-get -y install cmake build-essential libboost-all-dev git
-git clone -b Linux https://github.com/nicehash/nheqminer.git
+git clone -b master https://github.com/nicehash/nheqminer.git
 cd nheqminer/cpu_xenoncat/Linux/asm/
 sh assemble.sh
 cd ../../../Linux_cmake/nheqminer_cpu
